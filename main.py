edd90eca68f58d0ad13d0c4954126a51555c0f13
JBidlack <john.l.bidlack@gmail.com>
2023-02-04 20:57:12 -0500

    basic formatting changes

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
+"""Reddit Scraper"""
 #imports needed. The .env file will be .gitignored for security
-from dotenv.main import load_dotenv
-import praw
 import os
-import pandas as pd
 import datetime
 import time
+import pandas as pd
+import praw
+from dotenv.main import load_dotenv
 
 load_dotenv()
 
 #putting the connection info into a try/catch block in case of connection error
 try:
-    Reddit = praw.Reddit(
+    REDDIT = praw.Reddit(
         client_id = os.environ['client'],
         client_secret = os.environ['secret'],
         password = os.environ['pw'],
         user_agent = os.environ['user'] + " by u/JBiddyB",
         username = "JBiddyB"
     )
-except Exception as e:
+except ConnectionError() as e:
     print("Error connecting")
-    Reddit = None
+    REDDIT = None
 
-if Reddit:
-    source = Reddit.subreddit('antiwork').new(limit = 10000)
+if REDDIT:
+    source = REDDIT.subreddit('antiwork').new(limit = 10000)
     posts = {}
     keywords = {"bullsht", "meaningless", "aimless", "purposeless", "futile", "senseless work"}
 
@@ -32,32 +33,36 @@ if Reddit:
         'Body': [],
         'Posted': []
         }
-    count = 0
+    COUNT = 0
 #use a nested for loop to check each post title for the keywords
     for post in source:
-        count+=1
-        print(count)
+        COUNT+=1
+        print(COUNT)
         for keyword in keywords:
-            if (keyword in post.title.lower() or keyword in post.selftext.lower()) and post.id not in posts:
+            if (keyword in post.title.lower() or keyword
+            in post.selftext.lower()) and post.id not in posts:
                 posts[post.id] = post
                 data['Title'].append(post.title)
                 data['Body'].append(post.selftext)
                 data['Posted'].append(datetime.datetime.fromtimestamp(post.created_utc))
                 print(len(posts))
             if len(posts) == 10:
-                break     
-        if len(posts) == 10:
                 break
-# since the reddit API only allows 60 calls per minute, we sleep 
+        if len(posts) == 10:
+            break
+# since the REDDIT API only allows 60 calls per minute, we sleep
         time.sleep(1)
+
 # use pandas to create a dataframe of the information to be used for exporting to a csv
 # the csv is created, keeping the index True to make sorting easier
     df = pd.DataFrame(data)
     df.to_csv('results.csv', index = True)
 
-# Since the code will take a long time to run, the print statement is a simple method of letting me know the process is complete.
+# Since the code will take a long time to run,
+# the print statement is a simple method of letting me
+# know the process is complete.
+
     print(len(posts))
     print("Done")
 else:
     print("Connection problems. Please try again later")
-
